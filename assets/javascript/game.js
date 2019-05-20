aa6b30dbdca74831d98218d1d96f699e099e3f9d
Ronnie Larson <ronnielarson17@gmail.com>
2019-05-20 11:55:08 -0600

    Added a function that allows user to guess with keyboard and for the computer to randomly choose from the computer choices variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,3 +5,13 @@ var losses = 0;
 var numGuesses = 9;
 var guessChoices = [];
 
+document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+    
+
+}
