f9b6341e21f80309a70f00674e0f2704cd56095b
Ronnie Larson <ronnielarson17@gmail.com>
2019-05-20 18:18:52 -0600

    made an if statement that says if user guess is equal to computer guess the wins go up by 1 the number of guesses resets as well as the guess choices

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,5 +13,11 @@ document.onkeyup = function(event) {
 
     var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
     
+    if (options.indexOf(userGuess) > -1) {
 
-}
+        if (userGuess === computerGuess) {
+            wins++;
+            numGuesses = 9;
+            guessChoices = [];
+        }
+    }}
